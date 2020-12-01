894fa45d77b240d810691e19a3766736412c3c73
ANASTASIA_KONOVALOVA <polycolorart483@gmail.com>
2020-12-01 17:24:44 +0400

    Сделан кейс 1

--- src/main/java/pages/SearchPage.java
+++ src/main/java/pages/SearchPage.java
@@ -11,16 +11,23 @@ import static org.openqa.selenium.support.PageFactory.initElements;
 
 public class SearchPage {
     @FindBy(css="input.gLFyf.gsfi")
-    private WebElement searchInput;
+    private WebElement searchInput1;
 
     @FindBy(css="#search .g")
     public List<WebElement> results;
+
+    @FindBy(css="#cwos .qv3Wpe")
+    private WebElement searchInput2;
 
     public SearchPage(WebDriver driver) {
         initElements(driver, this);
     }
 
     public void search(String text){
-        searchInput.sendKeys(text, Keys.ENTER);
+        searchInput1.sendKeys(text, Keys.ENTER);
+    }
+
+    public void search1(String text){
+        searchInput2.sendKeys(text, Keys.ENTER);
     }
 }
--- src/test/java/tests/googleTests.java
+++ src/test/java/tests/googleTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import pages.DetailsPage;
@@ -34,22 +35,31 @@ public class googleTests {
     @BeforeEach
     public void setup(){driver.get ("http://www.google.com/");}
 
-    @Test @DisplayName("Поиск числа результатов")
+    @Test
+    @DisplayName("Проверка операций с целыми числами")
     public void test1() {
-        searchPage.search("selenium");
-        assertEquals(9, searchPage.results.size());
+        searchPage.search("Калькулятор");
+        driver.findElement(By.xpath("//div[@role='button' and @aria-label='открывающая скобка']")).click();
+        driver.findElement(By.xpath("//div[@role='button' and @jsname='N10B9']")).click();
+        driver.findElement(By.xpath("//div[@role='button' and @aria-label='сложение']")).click();
+        driver.findElement(By.xpath("//div[@role='button' and @jsname='lVjWed']")).click();
+        driver.findElement(By.xpath("//div[@role='button' and @aria-label='закрывающая скобка']")).click();
+        driver.findElement(By.xpath("//div[@role='button' and @aria-label='умножение']")).click();
+        driver.findElement(By.xpath("//div[@role='button' and @jsname='KN1kY']")).click();
+        driver.findElement(By.xpath("//div[@role='button' and @aria-label='вычитание']")).click();
+        driver.findElement(By.xpath("//div[@role='button' and @jsname='xAP7E']")).click();
+        driver.findElement(By.xpath("//div[@role='button' and @jsname='bkEvMb']")).click();
+        driver.findElement(By.xpath("//div[@role='button' and @aria-label='деление']")).click();
+        driver.findElement(By.xpath("//div[@role='button' and @jsname='Ax5wH']")).click();
+        driver.findElement(By.xpath("//div[@role='button' and @aria-label='равно']")).click();
+        /*driver.findElement(By.cssSelector("j93WEe.XRsWPe.MEdqYd")).click();*/
+        /*driver.findElement(By.cssSelector("input.cwos.qv3Wpe").sendKeys("(1 + 2) × 3 - 40 ÷ 5", Keys.ENTER));*/
+        assertEquals("(1 + 2) × 3 - 40 ÷ 5 =", driver.findElement(By.xpath("//span[@jsname='ubtiRe' and @class='vUGUtc']")).getText());
+        assertEquals("1", driver.findElement(By.xpath("//span[@jsname='VssY5c' and @class='qv3Wpe']")).getText());
     }
 
-    @Test @DisplayName("Проверка деталей ресурса")
-    public void test2() {
-        searchPage.search("selenium");
-        assertAll(
-                () -> assertEquals("Selenium", detailsPage.getName()),
-                () -> assertEquals("3.141.59 (14 ноября 2018 года)", detailsPage.getLastVersion()),
-                () -> assertEquals("Apache License 2.0", detailsPage.getLicense())
-        );
-
-    }
     @AfterAll
-    public static void teardown () {driver.quit();}
+    public static void teardown () {
+        driver.quit();
+    }
 }
